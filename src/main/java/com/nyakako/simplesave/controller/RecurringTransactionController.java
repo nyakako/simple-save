4546ea415900c22a35a4ab17834d4230b54fb055
nyakako <nyakako13@gmail.com>
2024-01-30 15:53:44 +0900

    定期入力の登録画面を追加-作業中

--- src/main/java/com/nyakako/simplesave/controller/RecurringTransactionController.java
+++ src/main/java/com/nyakako/simplesave/controller/RecurringTransactionController.java
@@ -1,21 +1,24 @@
 package com.nyakako.simplesave.controller;
 
+import java.time.LocalDate;
 import java.util.List;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 
 import com.nyakako.simplesave.model.RecurringTransaction;
+import com.nyakako.simplesave.service.CategoryService;
 import com.nyakako.simplesave.service.RecurringTransactionService;
 import org.springframework.web.bind.annotation.GetMapping;
-
 
 @Controller
 public class RecurringTransactionController {
     private final RecurringTransactionService recurringTransactionService;
+    private final CategoryService categoryService;
 
-    public RecurringTransactionController(RecurringTransactionService recurringTransactionService) {
+    public RecurringTransactionController(RecurringTransactionService recurringTransactionService, CategoryService categoryService) {
         this.recurringTransactionService = recurringTransactionService;
+        this.categoryService = categoryService;
     }
 
     @GetMapping("/settings/recurring-transactions")
@@ -26,5 +29,15 @@ public class RecurringTransactionController {
         model.addAttribute("content", "recurring-transactions");
         return "layout";
     }
-    
+
+    @GetMapping("/recurring-transactions/new")
+    public String newRecurringTransaction(Model model) {
+        LocalDate today = LocalDate.now();
+        model.addAttribute("today", today);
+        model.addAttribute("categories", categoryService.findAllCategories());
+        model.addAttribute("title", "新規明細登録 - simplesave");
+        model.addAttribute("content", "new-recurring-transaction");
+        return "layout";
+    }
+
 }
